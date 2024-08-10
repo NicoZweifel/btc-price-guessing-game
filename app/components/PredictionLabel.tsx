9669cabddfa6375cce012fc906dec40efd5ae2e7
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-08-10 13:49:03 +0200

    feat: improve Label

--- app/components/PredictionLabel.tsx
+++ app/components/PredictionLabel.tsx
@@ -14,25 +14,26 @@ function PredictionLabel({
   result?: EvaluatePredictionResult;
 } & ComponentProps<"div">) {
   const text = `Your guess currently resolves to: ${result?.value?.toString() ?? "unchanged."}`;
+  const date = new Date((prediction.timestamp + 120) * 1000);
+  date.setSeconds(0);
   return (
     <div
       {...props}
       className={cn(
         "py-3 px-10 flex-col rounded-lg text-black font-semibold ring-1 items-center justify-center flex gap-2",
-        prediction.direction == DIRECTION.UP ? "bg-green-700 ring-green-900" : "bg-red-700 ring-green-900",
+        prediction.direction == DIRECTION.UP
+          ? "bg-green-700 ring-green-900"
+          : "bg-red-700 ring-green-900",
         className,
       )}
     >
       <div className="flex flex-row gap-2 font-semibold text-lg">
         {prediction.direction == DIRECTION.UP ? <ArrowUp /> : <ArrowDown />}
-        {new Date((prediction.timestamp + 120) *1000).toLocaleDateString("DE-de",{
-          hour: "2-digit",
-          minute: "2-digit",
-        })}
+        {new Date(prediction.timestamp * 1000).toLocaleTimeString() +
+          " - " +
+          date.toLocaleTimeString()}
       </div>
-      <p className="font-bold" >
-        {text}
-      </p>
+      <p className="font-bold">{text}</p>
     </div>
   );
 }
